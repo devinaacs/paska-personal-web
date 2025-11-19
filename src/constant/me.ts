c23f654ddd3adb55c366529dab61419e1a7b8a56
devinacecilia <devina.silaen@centralmegakencana.com>
2025-11-19 21:40:22 +0700

    change linkedin url

--- src/constant/me.ts
+++ src/constant/me.ts
@@ -6,7 +6,7 @@ export const me = {
   role: "Data Analyst",
   heroTagline: "I help people finding insights through data",
   email: "paskalis97@gmail.com",
-  linkedinUrl: "https://www.linkedin.com/in/mikael-prapaskalis-517b69231/",
+  linkedinUrl: "https://www.linkedin.com/in/mikael-prapaskalis/",
   whatsappDisplay: "+6282118518423",
   whatsappNumber: "6282118518423",
   cvPath: "/files/resume_mikael.pdf",
